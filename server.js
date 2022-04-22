58f6bc6332fd662f8ec96e00e5cdf76badc71e57
Joel C <joel7921111@yahoo.com>
2022-04-21 18:22:48 -0600

    added helpers to format url, dates and pluralityof comments and points

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require("./config/connection");
 const path = require("path");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require("./utils/helpers");
 
 const sess = {
   secret: "Super secret secret",
@@ -15,16 +16,14 @@ const sess = {
   }),
 };
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 app.use(session(sess));
 
 // set up Handlebars.js as your app's template engine of choice
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
