18c4f366769efdf6c0c1a6e2208be9b2ab881d5b
Joel C <joel7921111@yahoo.com>
2022-04-24 10:35:14 -0600

    edit-post view. delete post button not working

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
+const withAuth = require('../../utils/auth');
 
 // get all posts
 router.get("/", (req, res) => {
@@ -89,12 +90,12 @@ router.get("/:id", (req, res) => {
 });
 
 // create a POST /api/posts
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -104,7 +105,7 @@ router.post("/", (req, res) => {
 });
 
 // upvote a post /api/posts/upvote
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   // make sure the session exists first
   if (req.session) {
     // pass session id along with all destructured properties on req.body
@@ -121,7 +122,7 @@ router.put("/upvote", (req, res) => {
 });
 
 // PUT /api/users/1 (update)
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -145,7 +146,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
