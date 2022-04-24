4f64905d327c12acff9ee0558a7fd3eeba71d928
Joel C <joel7921111@yahoo.com>
2022-04-24 11:57:33 -0600

    fixed db

--- models/Comment.js
+++ models/Comment.js
@@ -20,7 +20,7 @@ Comment.init(
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      // allowNull: false,
       references: {
         model: "user",
         key: "id",
@@ -28,7 +28,7 @@ Comment.init(
     },
     post_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      // allowNull: false,
       references: {
         model: "post",
         key: "id",
